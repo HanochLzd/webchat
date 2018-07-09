a0041306ea425abf7078530e48f99895be3e4b1f
15779163581@163.com <15779163581@163.com>
2018-07-09 15:03:07 +0800

    测试

---
+++ src/main/java/com/jxau/soft/webchat/controller/QRCodeController.java
@@ -0,0 +1,19 @@
+package com.jxau.soft.webchat.controller;
+
+import org.junit.Test;
+import org.python.util.PythonInterpreter;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Hanoch
+ */
+public class QRCodeController {
+
+
+    @Test
+    public void test1() {
+        PythonInterpreter interpreter = new PythonInterpreter();
+
+    }
+
+}
